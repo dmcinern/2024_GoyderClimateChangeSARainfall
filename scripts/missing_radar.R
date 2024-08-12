0a88635307324bb0a2136273ef3718a3540ca02f
David McInerney <a1065639@adelaide.edu.au>
2024-08-12 11:11:26 +0930

    determine which dates are missing from radar data

---
+++ scripts/missing_radar.R
@@ -0,0 +1,26 @@
+dirname = 'C:/Users/a1065639/Box/2024_GoyderClimateChangeSARainfall/8_Data_External/20240724_BOM_subdaily_rainfall_radar/BuckPk/prcp-c5/'
+
+files = list.files(dirname)
+
+date_str = strsplit(files[1],"[.]")[[1]][3]
+first.date = as.Date(date_str,format='%Y%m%d')
+
+date_str = strsplit(files[length(files)],"[.]")[[1]][3]
+last.date = as.Date(date_str,format='%Y%m%d')
+all_dates = seq(first.date,last.date,by='days')
+
+all_dates_str = format(all_dates,'%Y%m%d')
+
+length(all_dates)
+
+length(files)
+
+files_dates_str = c()
+for (f in 1:length(files)){
+  files_dates_str[f] = strsplit(files[f],"[.]")[[1]][3]
+}
+
+all_dates_str[which(!(all_dates_str %in% files_dates_str))]
+
+# output : "20190929" "20200330" "20200331" "20200725" "20200726" "20200727" "20200728"
+# note these dates are missing from aus unified radar archive 
